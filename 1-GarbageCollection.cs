57991c2aed47fa071102c86e0038621fc281e247
Pap Dávid <60669213+papdawin@users.noreply.github.com>
2022-09-15 09:14:42 +0200

    Add files via upload

---
+++ 1-GarbageCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test
+{
+    class Valami
+    {
+        public static int count = 0;
+        public static bool ki=false;
+        public Valami() {
+            count++;
+        }
+        ~Valami()
+        {
+            count--;
+            ki = true;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Valami a = new Valami();
+                Console.WriteLine(Valami.count);
+                if (Valami.ki) break;
+            }
+        }
+    }
+
+    
+}
