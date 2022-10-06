22b6963a175b84f5065a11fc887cce5b02155773
papdawin <papdawin@protonmail.com>
2022-10-06 09:31:37 +0200

    Enumos valami gyakorlas

---
+++ OraiGyak/Program.cs
@@ -0,0 +1,39 @@
+enum WeekDays
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+class Program
+{
+    private static Dictionary<WeekDays, string?> _napirend = new();
+    
+    public static void Main(string[] args)
+    {
+        _napirend[WeekDays.Monday] = "Weltraum surfen";
+        _napirend[WeekDays.Tuesday] = "Rake the leaves";
+        
+        Console.WriteLine("On which day would you like to add an activity? (Monday:1, Tuesday:2 ...)");
+        int day = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("What are we doing on "+ (WeekDays)day +" ?");
+        string activity = Console.ReadLine();
+        _napirend.Add((WeekDays)day-1, activity);
+        
+        for (int i = 0; i < 7; i++)
+        {
+            try
+            {
+                Console.WriteLine((WeekDays)i+"'s program: "+_napirend[(WeekDays)i]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("On "+(WeekDays)i+" we had no plans");
+            }
+        }
+    }
+}
